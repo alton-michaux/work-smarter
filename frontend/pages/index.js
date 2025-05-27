9c575fd8327a5e30d3aaaad6af558511499575f2
alton-michaux <alton.michaux@yahoo.com>
2025-05-26 22:52:17 -0400

    added copy to home, login and signup pages

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,7 +1,19 @@
+import Login from './login'
+import Signup from './signup';
+
 const Home = () => {
     return(
         <div>
-            <h1>PLACEHOLDER TEXT</h1>
+            <h1>Work Smarter</h1>
+
+            <p>Welcome to the Work Smarter app, where you can evaluate your skillsets efficiently.</p>
+            <p>To get started, please log in or sign up.</p>
+            <p>Use the links below:</p>
+            <ul>
+                <li><a href="/login">Login</a></li>
+                <li><a href="/signup">Signup</a></li>
+            </ul>
+            <p>Once logged in, you can access your dashboard to evaluate your skills.</p>
         </div>
     )
 };
--- frontend/pages/login.js
+++ frontend/pages/login.js
@@ -1,1 +1,22 @@
 // Authentication - Login
+export default function Login() {
+    return (
+        <div>
+            <h2>Login</h2>
+            <form>
+                <div>
+                    <label htmlFor="username">Username:</label>
+                    <input type="text" id="username" name="username" required />
+                </div>
+                <div>
+                    <label htmlFor="password">Password:</label>
+                    <input type="password" id="password" name="password" required />
+                </div>
+                <button type="submit">Login</button>
+            </form> 
+
+            <p>Don't have an account? <a href="/signup">Sign up here</a>.</p>
+            <a href="/">Back to Home</a>
+        </div>
+    )
+}
