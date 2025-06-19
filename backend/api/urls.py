79e42700d7837798631a9b3e9cc17a648005d730
alton-michaux <alton.michaux@yahoo.com>
2025-06-19 12:22:09 -0400

    added routes, serializers and views for Project and Task models

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,7 +1,9 @@
 from rest_framework.routers import DefaultRouter
-from .views import ResumeViewSet
+from .views import ResumeViewSet, TaskViewSet, ProjectViewSet
 
 router = DefaultRouter()
+router.register(r'tasks', TaskViewSet)
+router.register(r'projects', ProjectViewSet)
 router.register(r'resume', ResumeViewSet, basename='resume')
 
 urlpatterns = router.urls
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,10 +1,17 @@
 from rest_framework import viewsets, status
 from rest_framework.response import Response
 from rest_framework.parsers import MultiPartParser, FormParser
-from .models import Resume
-from .serializers import ResumeSerializer
+from .models import Resume, Project, Task
+from .serializers import ResumeSerializer, TaskSerializer, ProjectSerializer
 import openai
 
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
+class ProjectViewSet(viewsets.ModelViewSet):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
 class ResumeViewSet(viewsets.ViewSet):
     parser_classes = (MultiPartParser, FormParser)
 
