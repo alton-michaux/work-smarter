7c670b65521f9df4268dbe4ce967710d07c92cb9
Alton Michaux <alton.michaux@yahoo.com>
2025-08-02 16:49:35 -0400

    added tests for tasks

--- backend/test/conftest.py
+++ backend/test/conftest.py
@@ -1,7 +1,9 @@
 import pytest
 from rest_framework.test import APIClient
 from django.contrib.auth.models import User
+from api.models import Task
 from django.contrib.auth import get_user_model
+
 
 
 @pytest.fixture
@@ -22,6 +24,18 @@ def create_user(db):
     def make_user(username="testuser", email="testemail@email.com", password="testpass"):
         return User.objects.create_user(username=username, email=email, password=password)
     return make_user
+
+@pytest.fixture
+def create_task(db):
+    """
+    A factory function to create tasks easily in tests.
+    Usage:
+        task = create_taks(title="work", description="coding", begin_date="0000-00-00", is_done=True, user=1)
+    """
+    user = get_user
+    def make_task(title="work", description="coding", begin_date="0000-00-00", is_done=True, user=user):
+        return Task.objects.create(title=title, description=description, begin_date=begin_date, is_done=is_done, user=user)
+    return make_task
 
 @pytest.fixture
 def get_user():
--- backend/test/test_tasks.py
+++ backend/test/test_tasks.py
@@ -2,15 +2,13 @@ import pytest
 
 @pytest.mark.django_db
 def test_task_index(api_client, get_token):
-    token = get_token    
-    api_client.credentials(HTTP_AUTHORIZATION=f"Token {token}")
+    api_client.credentials(HTTP_AUTHORIZATION=f"Token {get_token}")
     response = api_client.get("/api/tasks/")
     assert response.status_code == 200
   
 @pytest.mark.django_db
-def test_task_create(api_client, get_token, get_user):  
-    token = get_token
-    api_client.credentials(HTTP_AUTHORIZATION=f"Token {token}")
+def test_task_create(api_client, get_token, get_user):
+    api_client.credentials(HTTP_AUTHORIZATION=f"Token {get_token}")
     user = get_user
     response = api_client.post("/api/tasks/", {
       "title": "Test Task",
@@ -19,13 +17,26 @@ def test_task_create(api_client, get_token, get_user):
       "is_done": False,
       "user": user.id
     })
-    print(f"response: {response.data}")
     assert response.status_code == 201
   
 @pytest.mark.django_db
-def test_task_update(api_client):
-  print("task_update")
+def test_task_update(api_client, get_token, get_user, create_task):
+    api_client.credentials(HTTP_AUTHORIZATION=f"Token {get_token}")
+    user = get_user
+    task = create_task(title="test_update", description="for testing update", begin_date="2025-07-31", is_done=True, user=user)
+    response = api_client.put(f"/api/tasks/{task.id}/", {
+      "title": "Test Task Update",
+      "description": "Task for testing update",
+      "begin_date": "2025-11-09",
+      "is_done": False,
+      "user": user.id
+    })
+    assert response.status_code == 200
   
 @pytest.mark.django_db
-def test_task_delete(api_client):
-  print("task_delete")
+def test_task_delete(api_client, get_token, get_user, create_task):
+    api_client.credentials(HTTP_AUTHORIZATION=f"Token {get_token}")
+    user = get_user
+    task = create_task(title="test_delete", description="for testing delete", begin_date="2025-08-01", is_done=False, user=user)
+    response = api_client.delete(f"/api/tasks/{task.id}/")
+    assert response.status_code == 204
