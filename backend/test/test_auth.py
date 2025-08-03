b4e129cde77532c9890ead66eafd9af58ceb79af
Alton Michaux <alton.michaux@yahoo.com>
2025-08-02 23:39:00 -0400

    added test for logout

--- backend/test/test_auth.py
+++ backend/test/test_auth.py
@@ -10,3 +10,12 @@ def test_login_success(api_client, create_user):
 def test_login_fail(api_client):
     response = api_client.post("/api/auth/login/", {"email": "bill@wrong.com", "password": f"wrong_password"}, format="json")
     assert response.status_code == 400
+    
+@pytest.mark.django_db
+def test_logout(api_client, create_user):
+    user = create_user(username="bob", email="bob@builder.com", password="canwefixit")
+    login_response = api_client.post("/api/auth/login/", {"email": user.email, "password": "canwefixit"}, format="json")
+    assert login_response.status_code == 200
+
+    logout_response = api_client.post("/api/auth/logout/")
+    assert logout_response.status_code == 200
