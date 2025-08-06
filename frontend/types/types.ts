676a7565475a15afe00f88e1c5241bc28dd48c66
Alton Michaux <alton.michaux@yahoo.com>
2025-08-05 23:50:46 -0400

    implemented a types file for centralized definitions of types for the frontend

---
+++ frontend/types/types.ts
@@ -0,0 +1,49 @@
+// types.ts
+
+export type Task = {
+  id: number;
+  title: string;
+  description?: string;
+  is_done: boolean;
+  priority: string;
+  category: string;
+  carry_over?: boolean;
+  is_subtask?: boolean;
+  project?: number;
+  begin_date: string;
+  end_date: string;
+  user: number;
+};
+
+export type Project = {
+  id: number;
+  name: string;
+  tasks: Task[];
+};
+
+export type User = {
+  id: number;
+  username: string;
+  email: string;
+};
+
+export type AuthTokens = {
+  access: string;
+  refresh: string;
+};
+
+export type RegisterPayload = {
+  username: string;
+  email: string;
+  password1: string;
+  password2: string;
+};
+
+export type LoginPayload = {
+  email: string;
+  password: string;
+};
+
+export type ApiError = {
+  [key: string]: string[]; // Example: { "email": ["This field is required."] }
+};
