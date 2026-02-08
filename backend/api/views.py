d8159f4eb08f17351cecfbe72a5b31ca83ac28ba
Alton Michaux <alton.michaux@yahoo.com>
2026-02-07 22:27:17 -0500

    implemented tons of parent/child task validations in serializer/viewset

--- backend/api/views.py
+++ backend/api/views.py
@@ -206,8 +206,15 @@ class TaskViewSet(viewsets.ModelViewSet):
 
         return queryset
 
-    def perform_create(self, serializer):
-        serializer.save(user=self.request.user)
+    def create(self, validated_data):
+        request = self.context.get("request")
+        validated_data["user"] = request.user
+        validated_data.pop("is_subtask", None)
+        return super().create(validated_data)
+
+    def update(self, instance, validated_data):
+        validated_data.pop("is_subtask", None)
+        return super().update(instance, validated_data)
         
     def destroy(self, request, *args, **kwargs):
         task = self.get_object()
@@ -225,9 +232,16 @@ class TaskViewSet(viewsets.ModelViewSet):
             rt.delete()
 
             return Response(status=status.HTTP_204_NO_CONTENT)
+        
+        Task.objects.filter(user=request.user, parent=task).update(parent=None, is_subtask=False)
 
         # default behavior: delete just this task row
         return super().destroy(request, *args, **kwargs)
+    
+    def get_serializer_context(self):
+        ctx = super().get_serializer_context()
+        ctx["user"] = self.request.user
+        return ctx
         
 class RecurringTaskViewSet(viewsets.ModelViewSet):
     serializer_class = RecurringTaskSerializer
