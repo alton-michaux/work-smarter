a5c950395f9178b1408def2abe311ed5e9ca2b2f
Alton Michaux <alton.michaux@yahoo.com>
2026-05-02 17:43:44 -0400

    fixed vercel build error

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,12 +3,8 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
-  purge: [],
   darkMode: 'class',
   theme: {
-    extend: {},
-  },
-  variants: {
     extend: {},
   },
   plugins: [
